f90bf6b7de6d83533f0d4378a279081e1f8e147e
MohammedJoda-stack <silicco@gmail.com>
2021-02-23 17:18:16 +0000

    Completed module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var items = _context.Items;
+            
+            return View("Index",items.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+         
+
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+
+            var items = _context.Items;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(item);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException er /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Item item)
+        {
+
+            var foundItem =  _context.Items.Find(item.Id);
+            if (foundItem == null)
+            {
+                
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                _context.Remove(foundItem);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (DbUpdateException er /* ex */)
+            {
+                return RedirectToAction("Index");
+            }
+
+           
+        }
+    }
+}
